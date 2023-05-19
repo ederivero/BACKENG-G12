be87214c62cd7b2bd2163fdb19e75750f21afc8c
Eduardo de Rivero <ederiveroman@gmail.com>
2023-05-18 21:27:59 -0500

    integracion front y back

--- dia-1/1-intro-flask.py
+++ dia-1/1-intro-flask.py
@@ -28,9 +28,16 @@ def gestionProductos():
     # request > me da toda la informacion de mi cliente
     print(request.method)
     if request.method == 'GET':
+        productos_existentes = []
+        for producto in productos:
+            if producto is None:
+                # evita que el codigo de a continuacion no se ejecute por este ciclo
+                continue
+            productos_existentes.append(producto)
+
         return {
             'message': 'Los productos son',
-            'content': productos
+            'content': productos_existentes
         }
     elif request.method == 'POST':
         print(request.json)
@@ -45,26 +52,44 @@ def gestionProductos():
         }
 
 
-@app.route('/producto/<int:id>', methods=['GET'])
+@app.route('/producto/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def gestionProducto(id):
     print(id)
+    resultado = None
+    for producto in productos:
+        if producto is None:
+            continue
+        if producto['id'] == id:
+            resultado = producto
+            break
+
+    if resultado is None:
+        return {
+            'message': 'No se encontro el producto a buscar'
+        }
+
     if request.method == 'GET':
-        resultado = None
-        for producto in productos:
-            if producto['id'] == id:
-                resultado = producto
-                break
+        return {
+            'message': 'El producto es',
+            'content': resultado
+        }
 
-        if resultado is None:
-            return {
-                'message': 'No se encontro el producto a buscar'
-            }
+    elif request.method == 'PUT':
+        data = request.json
+        # la posicion sera el valor del id - 1 porque cuando la pos es 0 el id es 1 y asi sucesivamente
+        productos[id - 1] = data
+        # el primer corchete indicara la posicion de la lista y el segundo corchete indicara la propiedad del diccionario
+        productos[id - 1]['id'] = id
+        return {
+            'message': 'Producto actualizado exitosamente'
+        }
 
-        else:
-            return {
-                'message': 'El producto es',
-                'content': resultado
-            }
+    elif request.method == 'DELETE':
+        productos[id - 1] = None
+
+        return {
+            'message': 'Producto eliminado exitosamente'
+        }
 
 
 if __name__ == "__main__":
