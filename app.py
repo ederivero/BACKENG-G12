7d3915ed20e25f1b46606fd7b510d1721e2b76a5
Eduardo de Rivero <ederiveroman@gmail.com>
2023-06-01 21:34:09 -0500

    estructura del proyecto

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restful import Api
+from dotenv import load_dotenv
+
+from os import environ  # muestra las variables de entorno
+# las variables de entorno son variables que estan presentes de manera GLOBAL en toda la maquina / servidor y es aca donde se suelen guardar las credenciales (a la bd, informacion a otras API's, mensajeria (emails), entro otros), credenciales sensibles que no deben ser expuestas
+
+# load_dotenv > carga todas las variables presentes en el archivo .env y las coloca como si fuesen variables de entorno, SIEMPRE debe ir en la primera linea del archivo principal del proyecto
+load_dotenv()
+
+# instancias
+app = Flask(__name__)
+api = Api(app)
+
+
+# el metodo get de los diccionarios intentara buscar esa llave y si no existe, retornara None, a diferencia de las [] (llaves) que si no encuentra emitira un error de tipo KeyError
+# el metodo .get() solamente se puede utilizar para devolver o visualizar los valores, mas no para asignar mientras que las llaves [] son para lectura y escritura
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = environ['DATABASE_URL']
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
