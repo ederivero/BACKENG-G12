31d7aef6bdda04141a285ef17e6d7150f79e79e2
Eduardo de Rivero <ederiveroman@gmail.com>
2023-06-07 21:11:06 -0500

    uso de jwt

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask_restful import Api
 from dotenv import load_dotenv
 from config import conexion
 from flask_migrate import Migrate
-from models.usuario_model import Usuario
-from models.publicacion_model import Publicacion
-from controllers.usuario_controller import RegistroController, LoginController
+from flask_jwt_extended import JWTManager
+from controllers.usuario_controller import RegistroController, LoginController, PerfilController
 
 from os import environ  # muestra las variables de entorno
 # las variables de entorno son variables que estan presentes de manera GLOBAL en toda la maquina / servidor y es aca donde se suelen guardar las credenciales (a la bd, informacion a otras API's, mensajeria (emails), entro otros), credenciales sensibles que no deben ser expuestas
@@ -16,6 +15,9 @@ load_dotenv()
 # instancias
 app = Flask(__name__)
 api = Api(app)
+app.config['JWT_SECRET_KEY'] = environ.get('SECRET_KEY')
+# busca en la variable config la variable JWT_SECRET_KEY > sera el secreto por el cual se generaran las tokens
+JWTManager(app)
 
 
 # el metodo get de los diccionarios intentara buscar esa llave y si no existe, retornara None, a diferencia de las [] (llaves) que si no encuentra emitira un error de tipo KeyError
@@ -31,6 +33,7 @@ Migrate(app, conexion)
 # declaramos las rutas
 api.add_resource(RegistroController, '/registro-usuario')
 api.add_resource(LoginController, '/login')
+api.add_resource(PerfilController, '/perfil')
 
 if __name__ == '__main__':
     app.run(debug=True)
