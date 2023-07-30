ba0f0acd7d6123f8860f5b71ba6c7dfe8ba31711
Eduardo de Rivero <eduardomanrique@ravn.co>
2023-07-29 20:45:17 -0500

    uso de docker con nodemon

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import express from "express";
-import mongoose from "mongoose";
-import { Producto } from "./models/productos.js";
+import express from 'express'
+import mongoose from 'mongoose'
+import { Producto } from './models/productos.js'
 
-const servidor = express();
-const PORT = process.env.PORT ?? 3000;
+const servidor = express()
+const PORT = process.env.PORT ?? 3000
 
-servidor.get("/", (req, res) => {
+servidor.get('/', (req, res) => {
   return res.status(200).json({
-    message: "Bienvenido a mi API con MongoDb",
-  });
-});
+    message: 'Bienvenido a mi API con MongoDb',
+    content: new Date().toISOString(),
+  })
+})
 
 servidor.listen(PORT, async () => {
-  console.log(`Servidor corriendo exitosamente en el puerto ${PORT}`);
-  await mongoose.connect(process.env.DATABASE_URL);
-  console.log("Base de datos conectada exitosamente");
-});
+  console.log(`Servidor corriendo exitosamente en el puerto ${PORT}`)
+  await mongoose.connect(process.env.DATABASE_URL)
+  console.log('Base de datos conectada exitosamente')
+})
